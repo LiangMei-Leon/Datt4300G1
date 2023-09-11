ccdb3659596900fcde6cb65032a0602412a96c16
Melocalyptic <philip.kim830@gmail.com>
2023-09-11 18:58:29 -0400

    Fix

---
+++ DATT4300G1/Assets/Scripts/FreezeManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeManager : MonoBehaviour
+{
+    public GameObject note;
+    public GameObject countdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NoteDone();
+    }
+
+    void NoteDone()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            note.SetActive(false);
+            PlayCountDown();
+        }
+    }
+
+    void PlayCountDown()
+    {
+        countdown.SetActive(true);
+    }
+}
